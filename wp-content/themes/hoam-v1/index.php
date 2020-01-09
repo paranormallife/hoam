a946c919c12822965b0c1b59143603fdae0750df
elliotshero <elliot@sherodesigns.com>
2020-01-09 11:02:00 -0500

    Hero and headings

--- wp-content/themes/hoam-v1/index.php
+++ wp-content/themes/hoam-v1/index.php
@@ -10,9 +10,10 @@
                 $thumb = get_the_post_thumbnail_url( $post->ID, 'full' );
                 echo '<div class="hero-container">';
                 echo    '<div class="hero-image" style="background-image: url('.$thumb.');">';
-                echo        '<h1>'.get_the_title().'</h1>';
+                echo        '<img src="'.$thumb.'" alt="'.get_the_title().'" />';
                 echo    '</div>';
                 echo '</div>';
+                echo '<div class="post-content">'.the_content().'</div>';
             } 
         } 
     ?>
